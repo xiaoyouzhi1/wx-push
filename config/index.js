a942ed1e84788555c40f972d5d29f9ac43a7b248
xiaoyouzhi1 <136344884+xiaoyouzhi1@users.noreply.github.com>
2023-06-12 22:06:16 +0800

    Update index.js

--- config/index.js
+++ config/index.js
@@ -1,10 +1,10 @@
 module.exports = {
-  appid: "wx71941f6106ae1072",
-  appsecret: "b4b2ad8a8d4a94e403831886ac10c646",
-  template_id: "q14lTqsDyK0N-KcZUs8UKHOl3GWn2uErDk-8hbm2ouQ",
-  touser: "oonWJ5slWuS2OrJdgCLM3GmNdxVk",
+  appid: "wxd4694c2f13569088",
+  appsecret: "e9dade933acc9e1952e169e873eccd3b",
+  template_id: "dI8V1HTzXoJPna7U6eSDbomG0qMeXed7aP7nt361FUI",
+  touser: "okOE66x2kYeJxQwXeqTKbgQI0TsU",
 
   key: "201c93c2e2b34bdeecdbf9f5265eb84e",
-  city: "邢台",
-  first_date: "2022-11-10",
+  city: "吉安",
+  first_date: "2023-15-23",
 };
