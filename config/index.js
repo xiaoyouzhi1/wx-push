b319f9346bb3edaf272c30a67bbd7597cc99f6c3
xiaoyouzhi1 <136344884+xiaoyouzhi1@users.noreply.github.com>
2023-06-13 13:19:24 +0800

    Update index.js

--- config/index.js
+++ config/index.js
@@ -2,7 +2,7 @@ module.exports = {
   appid: "wxd4694c2f13569088",
   appsecret: "e9dade933acc9e1952e169e873eccd3b",
   template_id: "dI8V1HTzXoJPna7U6eSDbomG0qMeXed7aP7nt361FUI",
-  touser: "okOE66xdulcjuW2uL8_L-htgqQBc",
+  touser: " okOE66xdulcjuW2uL8_L-htgqQBc",
 
   key: "201c93c2e2b34bdeecdbf9f5265eb84e",
   city: "泰和",
